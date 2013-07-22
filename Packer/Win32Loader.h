5ec8b90a0daac44cad41e42e4e1a0cbb927afda8
Inseok Lee <dlunch@gmail.com>
2013-07-22 15:58:21 +0900

    Fix datastorage being invalidated.

--- Packer/Win32Loader.h
+++ Packer/Win32Loader.h
@@ -7,12 +7,12 @@ class Win32Loader
 {
 private:
 	const Image &image_;
-	const DataStorage<Image> &imports_;
+	DataStorage<Image> imports_;
 	void *loadLibrary(const char *filename);
 	uint32_t getFunctionAddress(void *library, const char *functionName);
 	uint8_t *loadImage(const Image &image);
 public:
-	Win32Loader(const Image &image, const DataStorage<Image> &imports);
+	Win32Loader(const Image &image, DataStorage<Image> &&imports);
 	virtual ~Win32Loader() {}
 
 	void execute();
