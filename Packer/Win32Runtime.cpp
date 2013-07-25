597790e57fc464d5135ec673da01c2297cd5cd06
Inseok Lee <dlunch@gmail.com>
2013-07-25 19:52:40 +0900

    Use heapalloc and friends.

--- Packer/Win32Runtime.cpp
+++ Packer/Win32Runtime.cpp
@@ -1,19 +1,19 @@
 #include "Win32Runtime.h"
 
-#include <cstdlib>
+#include <windows.h>
 
 //TODO
 void* operator new(size_t num)
 {
-	return malloc(num);
+	return HeapAlloc(GetProcessHeap(), 0, num);
 }
 
 void operator delete(void *ptr)
 {
-	free(ptr);
+	HeapFree(GetProcessHeap(), 0, ptr);
 }
 
 void operator delete[](void *ptr)
 {
-	free(ptr);
+	HeapFree(GetProcessHeap(), 0, ptr);
 }
