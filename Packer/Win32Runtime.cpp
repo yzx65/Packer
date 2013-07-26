de845b65273798ca514c1697e1743907da9e8c37
Inseok Lee <dlunch@gmail.com>
2013-07-26 13:40:16 +0900

    Enable nodefaultlib.

--- Packer/Win32Runtime.cpp
+++ Packer/Win32Runtime.cpp
@@ -1,5 +1,6 @@
 #include "Win32Runtime.h"
 
+#include <cstdint>
 #include <windows.h>
 
 //TODO
@@ -28,5 +29,12 @@ extern "C"
 	int _purecall()
 	{
 		return 0;
+	}
+
+	void *memset(void *dst, int val, size_t size)
+	{
+		for(size_t i = 0; i < size; i ++)
+			*(reinterpret_cast<uint8_t *>(dst) + i) = static_cast<uint8_t>(val);
+		return dst;
 	}
 }
