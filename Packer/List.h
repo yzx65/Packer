132b7540cdb3558448268179c767afeabf6e9063
Inseok Lee <dlunch@gmail.com>
2013-07-26 13:37:14 +0900

    Add list move constructor, cleanup a bit.

--- Packer/List.h
+++ Packer/List.h
@@ -71,27 +71,47 @@ private:
 			return cnt;
 		}
 	};
+
+	void ensureHead_()
+	{
+		if(!head_)
+		{
+			head_ = new ListNodeBase();
+			head_->next = head_;
+			head_->prev = head_;
+		}
+	}
+
+	ListNode* appendNode_()
+	{
+		ensureHead_();
+		ListNode *item = new ListNode();
+		item->next = head_;
+		item->prev = head_->prev;
+		head_->prev->next = item;
+		head_->prev = item;
+
+		return item;
+	}
 public:
 	typedef ValueType value_type;
 	typedef ListIterator<const ListNodeBase, const ListNode, const value_type> const_iterator;
 	typedef ListIterator<ListNodeBase, ListNode, value_type> iterator;
-	List() 
-	{
-		head_ = new ListNodeBase();
-		head_->next = head_;
-		head_->prev = head_;
-	}
+	List()  : head_(nullptr) {}
 
 	~List()
 	{
-		ListNodeBase *item = head_->next;
-		while(item->next != head_)
+		if(head_)
 		{
-			ListNode *item_ = static_cast<ListNode *>(item);
-			item = item->next;
-			delete item_;
+			ListNodeBase *item = head_->next;
+			while(item->next != head_)
+			{
+				ListNode *item_ = static_cast<ListNode *>(item);
+				item = item->next;
+				delete item_;
+			}
+			delete head_;
 		}
-		delete head_;
 	}
 
 	List(const List &other)
@@ -109,29 +129,27 @@ public:
 		}
 	}
 
+	List(List &&other) : head_(other.head_)
+	{
+		other.head_ = nullptr;
+	}
+
 	void push_back(const value_type &data)
 	{
-		ListNode *item = new ListNode();
-		item->next = head_;
+		ListNode *item = appendNode_();
 		item->data = data;
-		item->prev = head_->prev;
-		head_->prev->next = item;
-		head_->prev = item;
 	}
 
 	void push_back(value_type &&data)
 	{
-		ListNode *item = new ListNode();
-		item->next = head_;
+		ListNode *item = appendNode_();
 		item->data = std::move(data);
-		item->prev = head_->prev;
-		head_->prev->next = item;
-		head_->prev = item;
 	}
 
 	template<typename InputIterator>
 	iterator insert(iterator at, InputIterator start, InputIterator end)
 	{
+		ensureHead_();
 		ListNode *position = static_cast<ListNode *>(at.item_);
 		for(; start != end; start ++)
 		{
@@ -147,26 +165,36 @@ public:
 
 	const_iterator begin() const
 	{
+		if(!head_)
+			return iterator(nullptr);
 		return const_iterator(head_->next);
 	}
 
 	const_iterator end() const
 	{
+		if(!head_)
+			return iterator(nullptr);
 		return const_iterator(head_);
 	}
 
 	iterator begin()
 	{
+		if(!head_)
+			return iterator(nullptr);
 		return iterator(head_->next);
 	}
 
 	iterator end()
 	{
+		if(!head_)
+			return iterator(nullptr);
 		return iterator(head_);
 	}
 
 	size_t size() const
 	{
+		if(!head_)
+			return 0;
 		size_t size = 0;
 		const ListNodeBase *item = head_;
 		while(item->next != head_)
