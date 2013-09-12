c4be9d79d077b59a31da9f3e45131398494a2afa
Inseok Lee <dlunch@gmail.com>
2013-09-12 10:51:32 +0900

    Remove unused.

--- Packer/Runtime.h
+++ Packer/Runtime.h
@@ -1,10 +1,5 @@
 #pragma once
 
-void* operator new(size_t num);
-void* operator new[](size_t num);
-void operator delete(void *ptr);
-void operator delete[](void *ptr);
-
 #ifdef _MSC_VER
 #include <type_traits> //for std::move. Just including <utility> makes static data entry, So we can't.
 #endif
