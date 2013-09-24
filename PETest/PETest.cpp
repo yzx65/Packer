bc24fc3b318d38aedd037b2ca870dc4a2740e3e6
Inseok Lee <dlunch@gmail.com>
2013-09-24 11:30:24 +0900

    Simple socket test.

--- PETest/PETest.cpp
+++ PETest/PETest.cpp
@@ -1,7 +1,22 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	std::cout << "Hello, World";
+	WSADATA wd = {0, };
+	int result = WSAStartup(MAKEWORD(2, 2), &wd);
+	std::cout << wd.szDescription;
+
+	addrinfo *info;
+	getaddrinfo("dlunch.net", "9008", NULL, &info);
+	std::cout << info;
+
+	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	connect(s, info->ai_addr, info->ai_addrlen);
+	send(s, "test", 4, 0);
+	closesocket(s);
 	return 0;
 }
