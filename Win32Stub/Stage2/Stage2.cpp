8478ee6c473f7f110cf8bf03de0fd1fe488b4cb8
Inseok Lee <dlunch@gmail.com>
2013-11-21 14:04:30 +0900

    Relocate stage2 before execution.

--- Win32Stub/Stage2/Stage2.cpp
+++ Win32Stub/Stage2/Stage2.cpp
@@ -4,41 +4,66 @@
 #include "../../Packer/Win32Loader.h"
 #include "../Win32Stub.h"
 
+Image *mainImage;
+List<Image> *importImages;
+
+void Execute();
+
 int Entry()
 {
+	Win32StubStage2Header *stage2Header;
 	Win32NativeHelper::get()->init();
+
+	mainImage = new Image();
+	importImages = new List<Image>();
+
 	PEFormat format;
 	SharedPtr<MemoryDataSource> selfImageSource = MakeShared<MemoryDataSource>(reinterpret_cast<uint8_t *>(WIN32_STUB_BASE_ADDRESS), 0);
 	format.load(selfImageSource, true);
 
-	Image mainImage;
-	List<Image> importImages;
 	for(auto &i : format.getSections())
 	{
 		if(i.name == WIN32_STUB_MAIN_SECTION_NAME)
-			mainImage = Image::unserialize(i.data, nullptr);
+			*mainImage = Image::unserialize(i.data, nullptr);
 		else if(i.name == WIN32_STUB_IMP_SECTION_NAME)
 		{
-			uint32_t count;
-			size_t off = 0;
+			uint32_t count = *reinterpret_cast<uint32_t *>(i.data->get());
+			size_t off = sizeof(count);
 			size_t size = 0;
-			{
-				SharedPtr<DataView> view = i.data->getView(off, 0);
-				count = *reinterpret_cast<uint32_t *>(view->get());
-				off += sizeof(count);
-			}
+
 			for(size_t j = 0; j < count; ++ j)
 			{
-				importImages.push_back(Image::unserialize(i.data->getView(off, 0), &size));
+				importImages->push_back(Image::unserialize(i.data->getView(off, 0), &size));
 				off += size;
 			}
 		}
+		else
+		{
+			size_t entryAddress = reinterpret_cast<size_t>(Entry);
+			if(i.baseAddress < entryAddress && entryAddress < i.baseAddress + i.size)
+				stage2Header = reinterpret_cast<Win32StubStage2Header *>(i.data->get());
+		}
 	}
 
+	//self relocation
+	uint8_t *newLocation = reinterpret_cast<uint8_t *>(Win32NativeHelper::get()->allocateVirtual(0, stage2Header->imageSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
+	uint8_t *stage2Start = reinterpret_cast<uint8_t *>(stage2Header) + sizeof(Win32StubStage2Header);
+	copyMemory(newLocation, stage2Start, stage2Header->imageSize);
+	uint64_t *relocationData = reinterpret_cast<uint64_t *>(stage2Start + stage2Header->imageSize);
+	for(size_t i = 0; i < stage2Header->numberOfRelocations; ++ i)
+		*reinterpret_cast<int32_t *>(newLocation + relocationData[i]) += -reinterpret_cast<int32_t>(stage2Start) + reinterpret_cast<int32_t>(newLocation);
+
+	typedef void (*ExecuteType)();
+	ExecuteType executeFunction = reinterpret_cast<ExecuteType>(reinterpret_cast<size_t>(Execute) - reinterpret_cast<int32_t>(stage2Start) + reinterpret_cast<int32_t>(newLocation));
+	executeFunction();
+
+	return 0;
+}
+
+void Execute()
+{
 	Win32NativeHelper::get()->unmapViewOfSection(reinterpret_cast<void *>(WIN32_STUB_BASE_ADDRESS));
 
-	Win32Loader loader(std::move(mainImage), std::move(importImages));
+	Win32Loader loader(std::move(*mainImage), std::move(*importImages));
 	loader.execute();
-	
-	return 0;
 }
