c72abe627f84a5ef54eb77eccce96461cc157c26
Inseok Lee <dlunch@gmail.com>
2013-12-07 14:56:18 +0900

    Split msvc runtime helper function.

---
+++ Win32/MSVCHelper.cpp
@@ -0,0 +1,28 @@
+#include "../Util/Util.h"
+
+extern "C" void *__cdecl memset(void *, int, size_t);
+#pragma intrinsic(memset)
+extern "C" void *__cdecl memcpy(void *dst, const void *src, size_t size);
+#pragma intrinsic(memcpy)
+
+extern "C"
+{
+	int _purecall()
+	{
+		return 0;
+	}
+
+#pragma function(memset)
+	void *__cdecl memset(void *dst, int val, size_t size)
+	{
+		setMemory(dst, val, size);
+		return dst;
+	}
+
+#pragma function(memcpy)
+	void *__cdecl memcpy(void *dst, const void *src, size_t size)
+	{
+		copyMemory(dst, src, size);
+		return dst;
+	}
+}
